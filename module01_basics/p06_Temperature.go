332205e71a7dae9d9533956e5c53707219b843ea
pandeypragati-tech <pragatipandey551@gmail.com>
2026-04-14 12:44:44 +0530

    Added Temperature conversion Program

---
+++ module01_basics/p06_Temperature.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var celsius float64
+
+	fmt.Print("Enter temperature in Celsius: ")
+	fmt.Scanln(&celsius)
+
+	fahrenheit := (celsius * 9 / 5) + 32
+	fmt.Println("Temperature in Fahrenheit:", fahrenheit)
+}
