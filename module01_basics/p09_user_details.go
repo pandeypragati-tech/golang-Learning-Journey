7f1804c0e5b0f6971b67510a5f47e35c42ad2ff6
pandeypragati-tech <pragatipandey551@gmail.com>
2026-04-14 13:06:46 +0530

    Added User Details Input Output Example

---
+++ module01_basics/p09_user_details.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	var age int
+
+	fmt.Print("Enter your name: ")
+	fmt.Scanln(&name)
+
+	fmt.Print("Enter your age: ")
+	fmt.Scanln(&age)
+
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+}
