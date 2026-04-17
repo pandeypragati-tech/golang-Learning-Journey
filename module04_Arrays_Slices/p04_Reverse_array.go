b8f83a3e30583dae78169d59fd77a0e8f0639d28
pandeypragati-tech <pragatipandey551@gmail.com>
2026-04-17 18:26:00 +0530

    Added array reversal using swapping

---
+++ module04_Arrays_Slices/p04_Reverse_array.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5}
+
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-1-i] = arr[len(arr)-1-i], arr[i]
+	}
+	fmt.Println(arr)
+}
